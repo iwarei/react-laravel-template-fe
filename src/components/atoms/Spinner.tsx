681e03a6653071cec1d74330f6bea188d2e94f16
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-09-08 00:55:08 +0900

    [ADD] 自動ログイン機能の追加

--- src/components/atoms/Spinner.tsx
+++ src/components/atoms/Spinner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Spinner = () => {
   return (
-    <div role="status">
+    <div role="status" className="text-center">
       <svg
         aria-hidden="true"
         className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
--- src/components/templates/PageTemplate.tsx
+++ src/components/templates/PageTemplate.tsx
@@ -1,27 +1,64 @@
-import React, { ReactNode, useContext, useEffect } from 'react';
+import React, { ReactNode, useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Navbar } from '../organism/Navbar';
 import { DismissableAlert } from '../atoms/Alert';
 import { AlertContext } from '../../context/AlertProvider';
 import { Spinner } from '../atoms/Spinner';
+import { IsAuthedContext } from '../../context/AuthProvider';
+import { useAuth } from '../../hooks/useAuth';
 
 type PageTemplateProps = {
   headerText?: string;
   children: ReactNode;
+  enableAutoLogin?: boolean;
   loading?: boolean;
 };
 
-export const PageTemplete: React.FC<PageTemplateProps> = ({
+export const PageTemplete = ({
   headerText,
   children,
+  enableAutoLogin = true,
   loading = false,
-}) => {
+}: PageTemplateProps) => {
   const location = useLocation();
   const { alert, setAlert } = useContext(AlertContext)!;
+  const { isAuthed } = useContext(IsAuthedContext)!;
+  const { autoLogin } = useAuth();
+  const [isLoading, setIsLoading] = useState<boolean>(enableAutoLogin);
 
   useEffect(() => {
     setAlert(undefined);
   }, [location.pathname]);
+
+  useEffect(() => {
+    if (enableAutoLogin && !isAuthed) {
+      // ログイン認証済み（セッションが生きている）場合、自動ログインする
+      (async () => {
+        await autoLogin({});
+        setIsLoading(false);
+      })();
+    }
+  }, []);
+
+  // 自動ログイン有効時、APIのレスポンスが返ってくるまで何も表示しない
+  if (isLoading && enableAutoLogin && !isAuthed) {
+    // return null;
+    return (
+      <>
+        <Navbar showButton={false} />
+        <main className="max-w-5xl mx-auto py-4 px-6">
+          {/* ロード中のメッセージとスピナーを表示する */}
+          <div
+            role="status"
+            className="fixed justify-center -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 z-20"
+          >
+            <Spinner />
+            <p>Now Loading...</p>
+          </div>
+        </main>
+      </>
+    );
+  }
 
   return (
     <>
