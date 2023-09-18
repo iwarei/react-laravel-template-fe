6f8757f5bf5a9458c3b513eb2220f8cc97434bed
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-09-19 01:16:15 +0900

    [FIX] ナビバーの不要な処理を削除

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -33,36 +33,32 @@ const NavbarLink = ({ link, text, onClick }: NavbarLinkProps) => {
   const navigate = useNavigate();
   const { isAuthed } = useContext(IsAuthedContext)!;
 
-  return (
-    <>
-      {!isAuthed ? (
-        <li>
-          <a
-            href={link}
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            {text}
-          </a>
-        </li>
-      ) : (
-        <li>
-          <button
-            type="button"
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            onClick={() => {
-              if (onClick) {
-                onClick();
-              }
-              if (link) {
-                navigate(link);
-              }
-            }}
-          >
-            {text}
-          </button>
-        </li>
-      )}
-    </>
+  return !isAuthed ? (
+    <li>
+      <a
+        href={link}
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+      >
+        {text}
+      </a>
+    </li>
+  ) : (
+    <li>
+      <button
+        type="button"
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+        onClick={() => {
+          if (onClick) {
+            onClick();
+          }
+          if (link) {
+            navigate(link);
+          }
+        }}
+      >
+        {text}
+      </button>
+    </li>
   );
 };
 
@@ -74,11 +70,6 @@ export const Navbar = ({ showButton = true }: NavbarProps) => {
 
   const { isAuthed } = useContext(IsAuthedContext)!;
   const { logout } = useAuth();
-
-  // アカウントボタン押下時のイベントハンドラ
-  const accountButtonHandler = () => {
-    navigate('/account');
-  };
 
   // ナビバー左サイドのロゴ・リンク押下時のイベントハンドラ
   const titleButtonHandler = () => {
