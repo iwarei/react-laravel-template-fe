b0389b0c34bf34c53320e3d0e78ff79597312dca
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-12-19 00:05:55 +0900

    [FIX] コンポーネント改修 (Table)

--- src/components/organisms/Table.tsx
+++ src/components/organisms/Table.tsx
@@ -1,40 +1,55 @@
 import React from 'react';
 
-export type HeaderProps = {
-  type?: 'text' | 'html' | undefined;
+export type HeaderCellProps = {
+  type?: 'text' | 'html';
   text?: string;
-  textColor?: string | undefined;
-  bgColor?: string | undefined;
-  className?: string | undefined;
-  rowSpan?: number | undefined;
-  colSpan?: number | undefined;
+  textColor?: string;
+  bgColor?: string;
+  width?: string;
+  weight?: 'bold' | 'normal' | 'lighter' | 'bolder';
+  className?: string;
+  rowSpan?: number;
+  colSpan?: number;
   html?: JSX.Element;
+  onClick?: (event: React.MouseEvent<HTMLTableCellElement>) => void;
+  onFocus?: React.FocusEventHandler<HTMLTableCellElement>;
+  id?: string | number;
+};
+
+export type HeaderProps = {
+  cellProps: HeaderCellProps[];
+  onClick?: (event: React.MouseEvent<HTMLTableRowElement>) => void;
+  onFocus?: React.FocusEventHandler<HTMLTableRowElement>;
+  id?: string | number;
 };
 
 export type CellProps = {
-  type?: 'text' | 'html' | undefined;
+  type?: 'text' | 'html';
   text?: string;
   bold?: boolean;
-  textColor?: string | undefined;
-  bgColor?: string | undefined;
-  className?: string | undefined; // type: textの場合のみ有効。
-  rowSpan?: number | undefined;
-  colSpan?: number | undefined;
+  textColor?: string;
+  bgColor?: string;
+  className?: string; // type: textの場合のみ有効。
+  rowSpan?: number;
+  colSpan?: number;
   html?: JSX.Element;
   onClick?: (event: React.MouseEvent<HTMLTableCellElement>) => void;
-  id?: string | number | undefined;
+  onFocus?: React.FocusEventHandler<HTMLTableCellElement>;
+  id?: string | number;
 };
 
 export type RowProps = {
   cellProps: CellProps[];
   onClick?: (event: React.MouseEvent<HTMLTableRowElement>) => void;
-  id?: string | number | undefined;
+  onFocus?: React.FocusEventHandler<HTMLTableRowElement>;
+  id?: string | number;
 };
 
 export type TableProps = {
   // select?: 'check' | 'radio' | undefined;
   // width?: string;
   // widthFlex?: number[];
+  className?: string;
   headerProps?: HeaderProps[];
   rowProps?: RowProps[];
   hover?: boolean;
@@ -46,6 +61,7 @@ export const Table = ({
   // select,
   // width,
   // widthFlex,
+  className,
   headerProps,
   rowProps,
   hover = true,
@@ -58,33 +74,52 @@ export const Table = ({
         shadow ? ' shadow-md' : ''
       } sm:rounded-lg`}
     >
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            {headerProps?.map((headerProp: HeaderProps) => {
-              if (headerProp.type === 'html') {
-                return headerProp.html;
-              }
-              return (
-                <th
-                  scope="col"
-                  className={headerProp.className ?? `px-6 py-4`}
-                  colSpan={headerProp.colSpan}
-                  rowSpan={headerProp.rowSpan}
-                >
-                  <span
-                    color={headerProp.textColor}
-                    style={{ backgroundColor: headerProp.bgColor }}
-                  >
-                    {headerProp.text}
-                  </span>
-                </th>
-              );
-            })}
-          </tr>
+      <table
+        className={`w-full table-fixed text-left text-gray-500 dark:text-gray-400 ${
+          className ?? ''
+        }`}
+      >
+        <thead className="text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          {headerProps?.map((row: HeaderProps, rowIndex: number) => {
+            return (
+              <tr key={`h-r${rowIndex + 1}`}>
+                {row.cellProps.map(
+                  (cell: HeaderCellProps, colIndex: number) => {
+                    return (
+                      <th
+                        scope="col"
+                        className={cell.className ?? `px-6 py-4`}
+                        colSpan={cell.colSpan}
+                        rowSpan={cell.rowSpan}
+                        style={{
+                          width: cell.width,
+                        }}
+                        key={`h-r${rowIndex + 1}-c${colIndex + 1}`}
+                        onFocus={cell.onFocus}
+                      >
+                        {cell.type === 'html' ? (
+                          cell.html
+                        ) : (
+                          <span
+                            style={{
+                              fontWeight: cell.weight,
+                              color: cell.textColor,
+                              backgroundColor: cell.bgColor,
+                            }}
+                          >
+                            {cell.text}
+                          </span>
+                        )}
+                      </th>
+                    );
+                  }
+                )}
+              </tr>
+            );
+          })}
         </thead>
         <tbody>
-          {rowProps?.map((rowProp: RowProps) => {
+          {rowProps?.map((rowProp: RowProps, rowIndex: number) => {
             return (
               <tr
                 className={`bg-white${
@@ -93,13 +128,15 @@ export const Table = ({
                   hover ? ' hover:bg-gray-50 dark:hover:bg-gray-600' : ''
                 }`}
                 onClick={rowProp.onClick}
+                onFocus={rowProp.onFocus}
                 id={
                   typeof rowProp.id === 'number'
                     ? rowProp.id.toString()
                     : rowProp.id
                 }
+                key={`r${rowIndex + 1}`}
               >
-                {rowProp.cellProps.map((cell: CellProps) => {
+                {rowProp.cellProps.map((cell: CellProps, colIndex: number) => {
                   if (cell.type === 'text' && cell.bold) {
                     return (
                       <th
@@ -116,17 +153,21 @@ export const Table = ({
                         rowSpan={cell.rowSpan}
                         colSpan={cell.colSpan}
                         onClick={cell.onClick}
+                        onFocus={cell.onFocus}
+                        key={`r${rowIndex + 1}-c${colIndex + 1}`}
                       >
                         {cell.text}
                       </th>
                     );
                   }
-                  if (cell.type === 'html') {
-                    return cell.html;
-                  }
                   return (
+                    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
                     <td
-                      className={cell.className ?? `px-6 py-4`}
+                      className={
+                        cell.type === 'html'
+                          ? cell.className
+                          : cell.className ?? `px-6 py-4`
+                      }
                       id={
                         typeof cell.id === 'number'
                           ? cell.id.toString()
@@ -135,13 +176,21 @@ export const Table = ({
                       rowSpan={cell.rowSpan}
                       colSpan={cell.colSpan}
                       onClick={cell.onClick}
+                      onFocus={cell.onFocus}
+                      key={`r${rowIndex + 1}-c${colIndex + 1}`}
                     >
-                      <span
-                        color={cell.textColor}
-                        style={{ backgroundColor: cell.bgColor }}
-                      >
-                        {cell.text}
-                      </span>
+                      {cell.type !== 'html' ? (
+                        <span
+                          style={{
+                            color: cell.textColor,
+                            backgroundColor: cell.bgColor,
+                          }}
+                        >
+                          {cell.text}
+                        </span>
+                      ) : (
+                        cell.html
+                      )}
                     </td>
                   );
                 })}
