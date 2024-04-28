161549238cf943726c2466bf7ce3ebf13ac2720e
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2024-04-28 13:36:09 +0900

    [FIX] ナビバーにドロップダウンを追加

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,8 +1,15 @@
-import React, { useContext, useState, useLayoutEffect, useEffect } from 'react';
+import React, {
+  useContext,
+  useState,
+  useLayoutEffect,
+  useEffect,
+  useRef,
+} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { IsAuthedContext } from '../../context/AuthProvider';
 import { useAuth } from '../../hooks/useAuth';
+import { useOutsideClick } from '../../hooks/useOutsideClick';
 
 type NavbarProps = {
   showButton?: boolean;
@@ -14,6 +21,12 @@ type NavbarLinkProps = {
   link?: string;
   text: string;
   onClick?: () => void;
+};
+
+type NavbarWithDropdownProps = {
+  text: string;
+  dropdownTexts: string[];
+  dropdownLinks: string[];
 };
 
 const PageTitleAndLogo = () => {
@@ -60,6 +73,77 @@ const NavbarLink = ({ link, text, onClick }: NavbarLinkProps) => {
       >
         {text}
       </button>
+    </li>
+  );
+};
+
+const NavbarWithDropdown = ({
+  text,
+  dropdownTexts,
+  dropdownLinks,
+}: NavbarWithDropdownProps) => {
+  const navigate = useNavigate();
+  const { isAuthed } = useContext(IsAuthedContext)!;
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useOutsideClick(dropdownRef, () => setIsOpen(false));
+
+  if (!isAuthed) return null;
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <li ref={dropdownRef}>
+      <button
+        id="dropdownNavbarLink"
+        data-dropdown-toggle="dropdownNavbar"
+        className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+        onClick={toggleDropdown}
+      >
+        {text}
+        <svg
+          className="w-2.5 h-2.5 ml-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+      {isOpen && (
+        <div
+          id="dropdownNavbar"
+          className="z-10 absolute font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+        >
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-400"
+            aria-labelledby="dropdownLargeButton"
+          >
+            {dropdownLinks.map((link, i) => (
+              <li key={`${link}-${dropdownTexts[i]}`}>
+                <button
+                  className="w-full text-start block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  onClick={() => {
+                    navigate(link);
+                  }}
+                >
+                  {dropdownTexts[i]}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </li>
   );
 };
